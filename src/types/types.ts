38919e1ae363a1a00363f6ee87edb288cb53d43d
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-11-28 01:12:26 +0200

    Added Prisma schema

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,21 @@
+export type ProductType = {
+    id: string;
+    title: string;
+    desc?: string;
+    img?: string;
+    price: number;
+    options?: { title: string; additionalPrice: number }[];
+};
+
+export type CartItemType = {
+    id: string;
+    title: string;
+    img?: string;
+    price: number;
+    optionTitle?: string;
+    quantity: number;
+};
+
 export type CartType = {
     products: CartItemType[];
     totalItems: number;
