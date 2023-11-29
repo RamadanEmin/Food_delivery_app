d07540912a9c7ca81a4795b7ed95169b5e5c9f70
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-11-29 11:47:48 +0200

    Fetch category

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,11 +1,26 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { ProductType } from '@/types/types';
+
+const getData = async (category: string) => {
+    const res = await fetch(`http://localhost:3000/api/products?cat=${category}`, {
+        cache: 'no-store'
+    });
+
+    if (!res.ok) {
+        throw new Error('Failed!');
+    }
+
+    return res.json();
+};
 
 type Props = {
     params: { category: string }
 }
 
 const CategoryPage = async ({ params }: Props) => {
+    const products:ProductType[] = await getData(params.category);
+
     return (
         <div className='flex flex-wrap text-red-500'>
             {products.map(item => (
