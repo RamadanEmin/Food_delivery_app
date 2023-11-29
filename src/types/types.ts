49d179fc7b1161dda0a0796e2fe519d136d0c972
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-11-29 09:10:52 +0200

    Fetch categories data

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,12 @@
+export type MenuType = {
+    id: string;
+    slug: string;
+    title: string;
+    desc?: string;
+    img?: string;
+    color: string;
+}[];
+
 export type ProductType = {
     id: string;
     title: string;
