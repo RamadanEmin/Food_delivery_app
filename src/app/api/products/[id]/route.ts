3817bfcaad68f47d60911a899c3bcb9d02753155
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-01 20:14:36 +0200

    Added Delete product api

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 import { prisma } from '@/utils/connect';
+import { getAuthSession } from '@/utils/auth';
 
 // GET SINGLE PRODUCT
 export const GET = async (req: NextRequest, { params }: { params: { id: string } }) => {
@@ -19,4 +20,28 @@ export const GET = async (req: NextRequest, { params }: { params: { id: string }
 
         return new NextResponse(JSON.stringify({ message: 'Something went wrong!' }), { status: 500 });
     }
+};
+
+// DELETE SINGLE PRODUCT
+export const DELETE = async (req: NextRequest, { params }: { params: { id: string } }) => {
+    const { id } = params;
+    const session = await getAuthSession();
+
+    if (session?.user.isAdmin) {
+        try {
+            await prisma.product.delete({
+                where: {
+                    id: id
+                },
+            });
+
+            return new NextResponse(JSON.stringify({ message: 'Product has been deleted!' }), { status: 200 });
+        } catch (error) {
+            console.log(error);
+
+            return new NextResponse(JSON.stringify({ message: 'Something went wrong!' }), { status: 500 });
+        }
+    }
+
+    return new NextResponse(JSON.stringify({ message: 'You are not allowed!' }), { status: 403 });
 };
