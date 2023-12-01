7bdd408c16911be4974a8e71207e02773342314d
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-01 17:50:25 +0200

    Added increase, decrease product and show total functionality

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,19 +1,36 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import { ProductType } from '@/types/types';
 
 const Price = ({ product }: { product: ProductType }) => {
+    const [total, setTotal] = useState(product.price);
+    const [quantity, setQuantity] = useState(1);
+    const [selected, setSelected] = useState(0);
+    
+    useEffect(() => {
+        if (product.options?.length) {
+            setTotal(quantity * product.price + quantity * product.options[selected].additionalPrice);
+        }
+    }, [quantity, selected, product]);
+
     return (
         <div className='flex flex-col gap-4'>
-            <h2 className='text-2xl font-bold'>$100</h2>
+            <h2 className='text-2xl font-bold'>${Number(total).toFixed(2)}</h2>
             {/* OPTIONS CONTAINER */}
             <div className='flex gap-4'>
                 {product.options?.length && product.options?.map((option, index) => (
                     <button
                         key={option.title}
                         className='min-w-[6rem] p-2 ring-1 ring-red-400 rounded-md'
+                        style={{
+                            background: selected === index ? 'rgb(248 113 113)' : 'white',
+                            color: selected === index ? 'white' : 'red'
+                        }}
+                        onClick={() => setSelected(index)}
                     >
-                    </button>
+                        {option.title}</button>
                 ))}
             </div>
 
@@ -23,9 +40,9 @@ const Price = ({ product }: { product: ProductType }) => {
                 <div className='flex justify-between w-full p-3 ring-1 ring-red-500'>
                     <span>Quantity</span>
                     <div className='flex gap-4 items-center'>
-                        <button>{'<'}</button>
-                        <span>0</span>
-                        <button>{'>'}</button>
+                        <button onClick={() => setQuantity(prev => (prev > 1 ? prev - 1 : 1))}>{'<'}</button>
+                        <span>{quantity}</span>
+                        <button onClick={() => setQuantity(prev => (prev < 9 ? prev + 1 : 9))}>{'>'}</button>
                     </div>
                 </div>
 
