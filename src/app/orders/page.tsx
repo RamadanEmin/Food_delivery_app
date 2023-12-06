b2ffd7fffdbfff09cd825eb1fd81e4e90f6f1c21
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-06 23:55:41 +0200

    Fetch orders

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,8 +1,33 @@
 'use client';
 
+import { useQuery } from '@tanstack/react-query';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
+import { OrderType } from '@/types/types';
+
 const OrdersPage = () => {
+    const { data: session, status } = useSession();
+
+    const router = useRouter();
+
+    if (status === 'unauthenticated') {
+        router.push('/login');
+    }
+
+    const { isLoading, error, data } = useQuery({
+        queryKey: ['orders'],
+        queryFn: () =>
+            fetch('http://localhost:3000/api/orders').then(
+                (res) => res.json(),
+            ),
+    });
+
+    if (isLoading || status === 'loading') {
+        return 'Loading...';
+    }
+
     return (
         <div className='p-4 lg:px-20 xl:px-40'>
             <table className='w-full border-separate border-spacing-3'>
@@ -16,12 +41,15 @@ const OrdersPage = () => {
                     </tr>
                 </thead>
                 <tbody>
-                        <tr>
-                            <td className='hidden md:block py-6 px-1'>111111</td>
-                            <td className='py-6 px-1'>06-12-2023</td>
-                            <td className='py-6 px-1'>100</td>
-                            <td className='hidden md:block py-6 px-1'>Peperoni pizza</td>
+                    {data.map((item: OrderType) => (
+                        <tr key={item.id} className={`${item.status !== 'delivered' ? 'bg-red-100' : 'bg-green-100'}`}>
+                            <td className='hidden md:block py-6 px-1'>{item.id}</td>
+                            <td className='py-6 px-1'>{item.createdAt.toString().slice(0, 10)}</td>
+                            <td className='py-6 px-1'>{item.price}</td>
+                            <td className='hidden md:block py-6 px-1'>{item.products[0].title}</td>
+                                <td className='py-6 px-1'>{item.status}</td>
                         </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
--- src/types/types.ts
+++ src/types/types.ts
@@ -16,6 +16,16 @@ export type ProductType = {
     options?: { title: string; additionalPrice: number }[];
 };
 
+export type OrderType = {
+    id: string;
+    userEmail: string;
+    price: number;
+    products: CartItemType[];
+    status: string;
+    createdAt: Date;
+    intent_id?: string;
+};
+
 export type CartItemType = {
     id: string;
     title: string;
