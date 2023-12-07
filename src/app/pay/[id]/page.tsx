0c464080f37554a0ce9421ae696b21b7e66cb016
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-07 23:24:29 +0200

    Added 'Checkout Form' component

--- src/app/pay/[id]/page.tsx
+++ src/app/pay/[id]/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 import { StripeElementsOptions, loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 
+import CheckoutForm from '@/components/CheckoutForm';
+
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 const PayPage = ({ params }: { params: { id: string } }) => {
