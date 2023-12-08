fb3992ed181342ee2b9875783915370668c4f186
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-08 21:02:53 +0200

    Fetch intent api

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,6 +1,32 @@
 'use client';
 
+import { useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+
 const SuccessPage = () => {
+    const serchParams = useSearchParams();
+    const payment_intent = serchParams.get('payment_intent');
+
+    const router = useRouter();
+
+    useEffect(() => {
+        const makeRequest = async () => {
+            try {
+                await fetch(`http://localhost:3000/api/confirm/${payment_intent}`, {
+                    method: 'PUT'
+                });
+
+                router.push('orders');
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        makeRequest();
+    }, [payment_intent, router]);
+
+
     return (
         <div>Payment successful. You are being redirected to the orders page. Please do not close the page.</div>
     );
