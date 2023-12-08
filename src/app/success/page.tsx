b369b2ffb72c9c460951ad7b209df206060d6ffe
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-12-08 17:32:42 +0200

    Added Success page

---
+++ src/app/success/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+const SuccessPage = () => {
+    return (
+        <div>Payment successful. You are being redirected to the orders page. Please do not close the page.</div>
+    );
+};
+
+export default SuccessPage;
